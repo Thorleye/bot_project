092969006852a8dff087c8f5875dd7372220f202
Thorley <thorley.eric@gmail.com>
2024-08-28 18:26:09 -0400

    date selector added

--- booking/booking.py
+++ booking/booking.py
@@ -1,6 +1,9 @@
 import booking.constants as const
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class Booking(webdriver.Chrome):
     def __init__(self, teardown=False):
@@ -18,12 +21,23 @@ class Booking(webdriver.Chrome):
         self.get(const.BASE_URL)
 
     def change_currency(self, currency=None):
-        currency_element = 'button[data-testid="header-currency-picker-trigger"]'
-        self.find_element(By.CSS_SELECTOR, currency_element).click()
-        selected_currency_element = 'div.c324bdcee4 ec7ca45eb7 ab9d1f83a2'
-        self.find_element(By.CSS_SELECTOR, selected_currency_element).click()
+        currency_element = '//*[@id="b2indexPage"]/div[2]/div/div/header/div/nav[1]/div[2]/span[1]/button/span'
+        self.find_element(By.XPATH, currency_element).click()
+        selected_currency_element = '//*[@id="b2indexPage"]/div[42]/div/div/div/div/div[2]/div/div[3]/div/div/div/ul[13]/li[2]/button/div/div'
+        self.find_element(By.XPATH, selected_currency_element).click()
 
     def select_location(self, location):
         search_field = self.find_element(By.XPATH, '//input[@id=":rh:"]')
         search_field.clear()
         search_field.send_keys(location)
+        search_field.send_keys(Keys.ESCAPE)
+        self.find_element(By.ID, "autocomplete-result-0").click()
+    
+    def select_dates(self, check_in, check_out): #YYYY-MM-DD
+        self.find_element(By.CSS_SELECTOR, f'span[data-date="{check_in}"]').click()
+        self.find_element(By.CSS_SELECTOR, f'span[data-date="{check_out}"]').click()
+
+    def select_adults(self):
+        self.find_element(By.CLASS_NAME, "a6391e882c").click()
+        self.find_element(By.XPATH, '//*[@id=":ri:"]/div/div[1]/div[2]/button[1]').click() #adult -1
+        self.find_element(By.XPATH, '//*[@id=":ri:"]/button').click() #done button
--- run.py
+++ run.py
@@ -2,5 +2,7 @@ from booking.booking import Booking
 
 with Booking(teardown=True) as bot:
     bot.landing_page()
-   # bot.change_currency()
     bot.select_location("New York City")
+    bot.select_dates(check_in="2024-09-25", check_out="2024-09-30")
+    bot.select_adults()
+    bot.change_currency()
